8b4abe22971e90ecba403baa4f3b0a8246b3a599
Igor Franca <igrfranca@gmail.com>
2026-01-11 19:19:12 -0300

    Teste arquivo hello world!

--- src/loiane/javaintermediario/HelloWorld.java
+++ src/loiane/javaintermediario/HelloWorld.java
@@ -12,5 +12,6 @@ public class HelloWorld {
 
     System.out.printf("Você digitou: %s\n", a);
     System.out.printf("Novamente, sua palavra é: %s", a);
+    System.out.printf("Outra vez, sua palavra é: %s", a);
   }
 }
