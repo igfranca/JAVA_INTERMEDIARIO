1ea93f333d1eaa75a455dc993fcf60b214f858a0
Igor Franca <igrfranca@gmail.com>
2026-01-11 19:21:17 -0300

    Segundo teste hello world!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
     String a = sc.nextLine();
 
     System.out.printf("Você digitou: %s\n", a);
-    System.out.printf("Novamente, sua palavra é: %s", a);
+    System.out.printf("Novamente, sua palavra é: %s\n", a);
     System.out.printf("Outra vez, sua palavra é: %s", a);
   }
 }
